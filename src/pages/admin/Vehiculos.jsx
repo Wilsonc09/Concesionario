e5dfc6cc8ca6614fd4893c1c67e3629cbcd83b24
Esteban <estebantolez@hotmail.com>
2022-02-23 09:24:19 -0500

    tablas dinamicas

--- src/pages/admin/Vehiculos.jsx
+++ src/pages/admin/Vehiculos.jsx
@@ -1,49 +1,114 @@
 import React, {useEffect, useState} from 'react'
 
+//simulando un json del backend:
+const vehiculoss = [
+  {
+    nombre: 'corola',
+    marca: 'Toyota',
+    modelo: 2014
+  },
+  {
+    nombre: 'Sandero',
+    marca: 'Renault',
+    modelo: 2020
+  },
+  {
+    nombre: 'corola',
+    marca: 'Toyota',
+    modelo: 2014
+  },
+  {
+    nombre: 'corola',
+    marca: 'Toyota',
+    modelo: 2014
+  },
+  {
+    nombre: 'corola',
+    marca: 'Toyota',
+    modelo: 2014
+  },
+]
+
 const Vehiculos = () => {
 
-  //Realizar un formulario que le pida su edad y muestre si es mayor de edad
-  const [edad, setEdad] = useState(0);
-  const [esMenor, setEsMenor] = useState(false);
+  const [mostrarTabla, setMostrarTabla]= useState(true);
+  const [vehiculos, setVehiculos] = useState([])
+  const [nomBoton, setnomBoton] = useState("Crear Nuevo");
 
   useEffect(()=>{
-    if (edad>=18){
-      setEsMenor(false)
+    //obtener lista de vehiculos desde el backend
+    setVehiculos(vehiculoss)
+  },[]);
+
+  useEffect(()=>{
+    if(mostrarTabla){
+      setnomBoton("Crear Nuevo")
     }
     else{
-      setEsMenor(true)
+      setnomBoton("Ver Todos")
     }
-
-  },[edad])
-
-
-  ////////////////////////////////
-
-  const [NombreVeh, setNombreVeh] =useState("") //una variable vacia
-
-  useEffect(()=>{
-     //una funcion que se ejecuta cuando cambia las dependencias
-    console.log("la var esta en:", NombreVeh);
-  },[NombreVeh])
-
-  const mandarBack =()=>{
-    console.log("el valor del nombre es", NombreVeh);
   }
+    ,[mostrarTabla]);
 
   return (
-    <form className='flex flex-col '>
-      <h2>Formulario de creacion de vehiculos</h2>
-      <input type="number" value={edad} onChange={(e)=>{setEdad(e.target.value)}} placeholder='edad' />
+    <div className='flex h-full w-full flex-col items-center justify-start'>
+      <div className='flex flex-col p-8'>
+        <h2 className='text-3xl text-gray-900 font-extrabold'>Pagina de administración de vehiculos</h2>
+      
+        <button onClick={()=>{setMostrarTabla(!mostrarTabla)}} className=' text-white bg-indigo-500 p-5 rounded-full w-28 self-end'>
+          {nomBoton}
+        </button>
+      </div>
+      
+      {mostrarTabla ? <TablaVehiculos listaVehiculos={vehiculos}/> : <Formulario/>}
+      
+    </div>
+  )
+}
 
-      <input type="text" onChange={(e)=>{setNombreVeh(e.target.value)}} placeholder='nombre del vehiculo'/>
-      <input type="text" placeholder='marca'/>
-      <input type="text" placeholder='modelo'/>
-      <button onClick={mandarBack} type='button' className=' bg-indigo-500 text-white '>Enviar</button>
+const TablaVehiculos =( {listaVehiculos})=>{
+  useEffect(()=>{
+    console.log("esta es la informacion que llega de los vehiculos", listaVehiculos);
+  },[listaVehiculos])
+  return(
+    <div className='flex flex-col items-center justify-center'>
+      <h2 className='text-2xl font-extrabold text-gray-800'>Todos los vehiculos</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Nombre del Vehiculo</th>
+            <th>Marca del vehiculo</th>
+            <th>Modelo del vehiculo</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listaVehiculos.map((vehiculo)=>{
+            return(
+              <tr>
+                <td>{vehiculo.nombre}</td>
+                <td>{vehiculo.marca}</td>
+                <td>{vehiculo.modelo}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
+  )
+}
 
-      {esMenor ? <span className=' text-3xl text-red-500'>Usted es menor de edad</span>:
-      <span className=' text-3xl text-green-500'>Usted es mayor de edad</span>
-      }
-    </form>
+const Formulario =()=>{
+  return(
+    <div className='felx flex-col items-center justify-center'>
+      <h2 className='text-2xl font-extrabold text-gray-800'>Crear Nuevo Vehiculo</h2>
+      <form className='grid grid-cols-2'>
+        <input className='bg-gray-50 border border-gray-600 rounded-lg m-2' type="text" />
+        <input className='bg-gray-50 border border-gray-600 rounded-lg m-2' type="text" />
+        <input className='bg-gray-50 border border-gray-600 rounded-lg m-2' type="text" />
+        <input className='bg-gray-50 border border-gray-600 rounded-lg m-2' type="text" />
+        <button className='col-span-2 bg-green-500 p-2 rounded-full shadow-md hover:bg-green-700'>Guardar</button>
+      </form>
+    </div>
   )
 }
 
