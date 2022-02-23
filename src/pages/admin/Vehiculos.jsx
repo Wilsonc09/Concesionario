f137c938c8d5a882bd215ba300c3f94cd54a5aa7
Esteban <estebantolez@hotmail.com>
2022-02-23 11:04:03 -0500

    validacion con JS no recomendado

--- src/pages/admin/Vehiculos.jsx
+++ src/pages/admin/Vehiculos.jsx
@@ -108,15 +108,21 @@ const TablaVehiculos =( {listaVehiculos})=>{
 }
 
 const Formulario =({fncMostrarTabla, listaVehiculos, fcnAgregarVehiculo})=>{
-  const [nombre, setNombre] =useState();
-  const [marca, setMarca] =useState();
-  const [modelo, setModelo] =useState();
+  const [nombre, setNombre] =useState('');
+  const [marca, setMarca] =useState('');
+  const [modelo, setModelo] =useState('');
 
   const enviarBack = ()=>{
     console.log("nombre",nombre,'marca',marca,'modelo',modelo);
-    toast.success('Vehiculo creado con exito');
-    fncMostrarTabla(true)
-    fcnAgregarVehiculo([...listaVehiculos,{nombre:nombre, marca:marca, modelo:modelo}]) //se agrega un nuevo vehiculo
+    if(nombre==='' || marca==='' || modelo===''){
+      toast.error('ingrese bien')
+    }
+    else{
+      toast.success('Vehiculo creado con exito');
+      fncMostrarTabla(true)
+      fcnAgregarVehiculo([...listaVehiculos,{nombre:nombre, marca:marca, modelo:modelo}]) //se agrega un nuevo vehiculo
+    }
+    
   }
   return(
     <div className='felx flex-col items-center justify-center'>
@@ -126,11 +132,13 @@ const Formulario =({fncMostrarTabla, listaVehiculos, fcnAgregarVehiculo})=>{
           Nombre del vehiculo
           <input name='nombre' className='bg-gray-50 border border-gray-600 rounded-lg m-2' type="text" placeholder='Corola' 
           value={nombre}
+          required
           onChange={(e)=>{setNombre(e.target.value)}}/>
         </label>
         <label className='flex flex-col' htmlFor="marca">
           Marca del vehiculo
-          <select 
+          <select
+          required 
           value={marca}
           onChange={(e)=>{setMarca(e.target.value)}}
           className='bg-gray-50 border border-gray-600 rounded-lg m-2'>
@@ -149,13 +157,14 @@ const Formulario =({fncMostrarTabla, listaVehiculos, fcnAgregarVehiculo})=>{
           min={1992}
           max={2022}
           value={modelo}
+          required
           onChange={(e)=>{setModelo(e.target.value)}}
            />
         </label>
         
         <button 
         onClick={()=>{enviarBack()}}
-        type='button'
+        type='submit'
         className='col-span-2 bg-green-500 p-2 rounded-full shadow-md hover:bg-green-700'>Guardar</button>
       </form>
     </div>
